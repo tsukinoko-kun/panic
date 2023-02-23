51fcc6a24b13323a490f54ab206ed3518dbd1e7e
Frank-Mayer <git@frank-mayer.io>
2023-02-23 19:08:20 +0100

    added test for panic throw function

--- test/Panic.test.js
+++ test/Panic.test.js
@@ -1,15 +1,15 @@
-const { Panic } = require("..")
+const { Panic, panic } = require("..")
 
-test("name", async () => {
+test("name", () => {
     expect(new Panic().name).toBe("Panic")
 })
 
-test("message", async () => {
+test("message", () => {
     const msg = Math.random().toString(36).substring(2)
     expect(new Panic(msg).message).toBe(msg)
 })
 
-test("stack", async () => {
+test("stack", () => {
     expect(new Panic().stack).toBeTruthy()
 
     expect(new Panic().stack).toMatch(/Panic\.test\.js/)
@@ -19,4 +19,9 @@ test("stack", async () => {
 
         expect(p.stack).toEqual(p.stack)
     }
+})
+
+test("panic function", () => {
+    expect(() => panic("meep")).toThrow(Panic)
+    expect(() => panic("meep")).toThrow("meep")
 })
