03212e0a9d90f42b154a830c866b27467cd9aae1
Frank-Mayer <git@frank-mayer.io>
2023-02-23 22:11:39 +0100

    added throw performance test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+/**
+ * This is a blazingly fast reimplementation of the `Error` class.
+ * In most cases, you don't need the call stack of an `Error`.
+ * This implementation is a lot faster than the original `Error` class because the collection of the call stack is extremely expensive and `Panic` only collects the call stack when you access its `stack` property.
+ *
+ * | Name        | Ops/sec     | Relative margin of error |
+ * | ----------- | ----------- | ------------------------ |
+ * | new Error() | 231,460     | ±8.64%                   |
+ * | new Panic() | 360,287,200 | ±0.92%                   |
+ */
 export class Panic implements Error {
     /** @internal */
     protected _stack: string | undefined = undefined
@@ -23,6 +33,15 @@ export class Panic implements Error {
     }
 }
 
+/**
+ *
+ * @param message The message of the panic.
+ *
+ * | Name              | Ops/sec   | Relative margin of error |
+ * | ----------------- | --------- | ------------------------ |
+ * | throw new Error() | 139,958   | ±0.38%                   |
+ * | panic()           | 3,247,902 | ±0.29%                   |
+ */
 export const panic = (message: string) => {
     // eslint-disable-next-line no-restricted-syntax
     throw new Panic(message)
